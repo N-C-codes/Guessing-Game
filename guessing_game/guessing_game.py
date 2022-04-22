38320824ba81b778bac4b1a0438de313a4c13c43
N-C-codes <che.nj.75357@gmail.com>
2022-04-22 22:33:05 +0100

    Refactoring into OOP; removing a repeated message

--- guessing_game/guessing_game.py
+++ guessing_game/guessing_game.py
@@ -1,112 +1,123 @@
 from random import randint
 
-def show_instructions():
-    """Shows the game instructions to the player."""
+class Guessing_Game():
 
-    print("Welcome to the number guessing game!\n")
-    print("Instructions:")
-    print("- Select a lower and upper bound for a number.")
-    print("- Select how many guesses you would like to have. You cannot have more guesses than possible numbers.\n")
-    print("The computer will select a random number between the upper and lower bounds (inclusive).")
-    print("Can you guess the number before you run out of guesses? :)\n")
+    def __init__(self):
+        pass
 
-def validate_input():
-    """Validates the player's input to make sure that an integer value is given."""
+    def show_instructions(self):
+        """Shows the game instructions to the player."""
 
-    try:
-        # Modify to allow decimals that are whole numbers, e.g. 10.0:
-        return int(input()) 
-    except ValueError: 
-        print("Invalid input. Please enter a whole number.")
-        return validate_input() # Function is called as many times as is necessary to obtain valid input.
+        print("Welcome to the number guessing game!\n")
+        print("Instructions:")
+        print("- Select a lower and upper bound for a number.")
+        print("- Select how many guesses you would like to have. You cannot have more guesses than possible numbers.\n")
+        print("The computer will select a random number between the upper and lower bounds (inclusive).")
+        print("Can you guess the number before you run out of guesses? :)\n")
 
-def select_bounds():
-    """Enables a player to select lower and upper bounds to guess between."""
+    def validate_input(self):
+        """Validates the player's input to make sure that an integer value is given."""
 
-    print("Select a lower bound:")
-    lower = validate_input() 
-    print("Select an upper bound:")
-    upper = validate_input() 
+        try:
+            # Future feature: Modify to allow decimals that are whole numbers, e.g. 10.0:
+            return int(input()) 
+        except ValueError: 
+            print("Invalid input. Please enter a whole number.")
+            return self.validate_input() # Function is called as many times as is necessary to obtain valid input.
 
-    while lower >= upper: 
-        print("The upper bound must be greater than the lower bound. Please enter different bounds.\n")
+    def select_bounds(self):
+        """Enables a player to select lower and upper bounds to guess between."""
+
         print("Select a lower bound:")
-        lower = validate_input()
+        lower = self.validate_input() 
         print("Select an upper bound:")
-        upper = validate_input()
-    
-    return (lower, upper)
+        upper = self.validate_input() 
 
-def select_attempt_number(range):
-    """Allows the player to select the number of guesses they can have."""
+        while lower >= upper: 
+            print("The upper bound must be greater than the lower bound. Please enter different bounds.\n")
+            print("Select a lower bound:")
+            lower = self.validate_input()
+            print("Select an upper bound:")
+            upper = self.validate_input()
+        
+        return (lower, upper)
 
-    print("How many guesses would you like to have?")
-    attempts = validate_input()
+    def select_attempt_number(self, range):
+        """Allows the player to select the number of guesses they can have."""
 
-    while attempts < 1 or attempts > range:
-        if attempts < 1:
-            print("You need to have at least 1 guess.")
-        elif attempts > range:
-            print("You cannot have more guesses than possible numbers!") 
+        self.range = range
 
-        print("Please select a different number of guesses.\n")
-        attempts = validate_input()
-    
-    return attempts
+        print("How many guesses would you like to have?")
+        attempts = self.validate_input()
 
-def guess_the_number(lower, upper):
-    """Allows the player to guess a number in the range until they guess correctly or run out of guesses."""
+        while attempts < 1 or attempts > self.range:
+            if attempts < 1:
+                print("You need to have at least 1 guess.")
+            elif attempts > self.range:
+                print("You cannot have more guesses than possible numbers!") 
 
-    range = upper - lower + 1 # Possible numbers that can be guessed.
-    attempts = select_attempt_number(range) # Player can guess how many guess attempts they would like
-    answer = randint(lower, upper) # Computer chooses random number in the range for the player to guess
-    attempt_counter = 0
-    while attempt_counter < attempts:
-        print("Guess a number:")
-        guess = validate_input()
+            print("Please select a different number of guesses.\n")
+            attempts = self.validate_input()
+        
+        return attempts
 
-        if guess < lower or guess > upper:
-            print(f"Please guess a number between {lower} and {upper}!")
-        else:
-            attempt_counter += 1
-            if guess == answer:
-                print("Congratulations! You guessed correctly!\n")
-                break
+    def guess_the_number(self, lower, upper):
+        """Allows the player to guess a number in the range until they guess correctly or run out of guesses."""
+
+        self.lower = lower
+        self.upper = upper
+        
+        range = self.upper - self.lower + 1 # Possible numbers that can be guessed.
+        attempts = self.select_attempt_number(range) # Player can guess how many guess attempts they would like
+        answer = randint(self.lower, self.upper) # Computer chooses random number in the range for the player to guess
+        attempt_counter = 0
+        while attempt_counter < attempts:
+            print("Guess a number:")
+            guess = self.validate_input()
+
+            if guess < self.lower or guess > self.upper:
+                print(f"Please guess a number between {lower} and {upper}!")
             else:
-                direction = "high" if guess > answer else "low"
-                print(f"Incorrect: too {direction}!")
-                remaining = attempts - attempt_counter 
+                attempt_counter += 1
+                if guess == answer:
+                    print("Congratulations! You guessed correctly!\n")
+                    break
+                else:
+                    direction = "high" if guess > answer else "low"
+                    print(f"Incorrect: too {direction}!")
+                    remaining = attempts - attempt_counter 
 
-                if attempts > 1: 
-                    plural_att = "es" * (attempt_counter > 1) #Plural "es" suffix if the user has made more than one guess.
-                    plural_rem = "es" * (remaining == 0 or remaining > 1) #Plural "es" suffix if the user has more than one guess or no guesses left.
-                    print(f"You have used {attempt_counter} guess{plural_att}. You have {remaining} guess{plural_rem} remaining.") #Ensures a grammatically correct message.
+                    if attempts > 1 and attempt_counter != attempts:
+                        plural_att = "es" * (attempt_counter > 1) #Plural "es" suffix if the user has made more than one guess.
+                        plural_rem = "es" * (remaining == 0 or remaining > 1) #Plural "es" suffix if the user has more than one guess or no guesses left.
+                        print(f"You have used {attempt_counter} guess{plural_att}. You have {remaining} guess{plural_rem} remaining.") #Ensures a grammatically correct message.
 
-    if guess != answer and attempt_counter == attempts:           
-        print(f"You have used up all your guesses! The correct answer was {answer}.\n") 
+        if guess != answer and attempt_counter == attempts:           
+            print(f"You have used up all your guesses! The correct answer was {answer}.\n") 
 
-def play_again_or_not():
-    """Allows the player to decide whether or not they want to play the game again."""
+    def play_again_or_not(self):
+        """Allows the player to decide whether or not they want to play the game again."""
 
-    print("You have finished playing the guessing game.")
-    again = input("Would you like to play again?").lower() #Allows case-insensitivity.
+        print("You have finished playing the guessing game.")
+        again = input("Would you like to play again?").lower() #Allows case-insensitivity.
 
-    while again != "yes" and again != "no":
-        print("Invalid response. Please answer either 'yes' or 'no'.")
-        again = input("Would you like to play again?").lower()
-    
-    return again
+        while again != "yes" and again != "no":
+            print("Invalid response. Please answer either 'yes' or 'no'.")
+            again = input("Would you like to play again?").lower()
+        
+        return again
 
 if __name__ == "__main__":
     
-    show_instructions()
+    new_player = Guessing_Game()
+    new_player.show_instructions()
     while True:
-        bounds = select_bounds() # The bounds between which the player is guessing a number.
+        bounds = new_player.select_bounds() # The bounds between which the player is guessing a number.
         lower = bounds[0]
         upper = bounds[1]
 
-        guess_the_number(lower, upper) # Player tries to guess the number chosen by the computer between the bounds.
+        new_player.guess_the_number(lower, upper) # Player tries to guess the number chosen by the computer between the bounds.
 
-        again = play_again_or_not()
+        again = new_player.play_again_or_not()
         if again == "no":
             break
